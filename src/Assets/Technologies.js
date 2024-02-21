09ac722475842745abd121759fc82d53a0bc263d
Radwanbaba13 <radwangbaba@gmail.com>
2024-02-21 14:28:58 -0500

    Added Tech List

---
+++ src/Assets/Technologies.js
@@ -0,0 +1,102 @@
+import * as svgs from "../Assets/Logos/index";
+
+export const languages = [
+  {
+    name: "JavaScript",
+    logo: svgs.javascript,
+  },
+  {
+    name: "TypeScript",
+    logo: svgs.typescript,
+  },
+  {
+    name: "Java",
+    logo: svgs.java,
+  },
+  {
+    name: "Python",
+    logo: svgs.python,
+  },
+  {
+    name: "C#",
+    logo: svgs.csharp,
+  },
+  {
+    name: "C++",
+    logo: svgs.cplusplus,
+  },
+];
+export const frameworks = [
+  {
+    name: "Next.js",
+    logo: svgs.nextjs,
+  },
+  {
+    name: "ASP.NET",
+    logo: svgs.dotnetcore,
+  },
+  {
+    name: "Node.js",
+    logo: svgs.nodejs,
+  },
+  {
+    name: "React",
+    logo: svgs.react,
+  },
+  {
+    name: "Angular",
+    logo: svgs.angularjs,
+  },
+  {
+    name: "Tailwind CSS",
+    logo: svgs.tailwindcss,
+  },
+  {
+    name: "Bootstrap",
+    logo: svgs.bootstrap,
+  },
+];
+export const databases = [
+  {
+    name: "SQL",
+    logo: svgs.mysql,
+  },
+  {
+    name: "MongoDB",
+    logo: svgs.mongodb,
+  },
+  {
+    name: "Neo4j",
+    logo: svgs.neo4j,
+  },
+];
+export const machineLearning = [
+  {
+    name: "PyTorch",
+    logo: svgs.pytorch,
+  },
+  {
+    name: "TensorFlow",
+    logo: svgs.tensorflow,
+  },
+];
+export const testingFrameworks = [
+  {
+    name: "Jest",
+    logo: svgs.jest,
+  },
+  {
+    name: "Cypress",
+    logo: svgs.cypress,
+  },
+];
+export const tools = [
+  {
+    name: "Docker",
+    logo: svgs.docker,
+  },
+  {
+    name: "AWS",
+    logo: svgs.amazonwebservices,
+  },
+];
